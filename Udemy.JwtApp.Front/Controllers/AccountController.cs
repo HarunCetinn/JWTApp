dea4d60dfdeb14b19d3a164500e56bedc695e924
HarunCetinn <79652070+HarunCetinn@users.noreply.github.com>
2022-08-01 17:07:11 +0300

    Frontend

---
+++ Udemy.JwtApp.Front/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Udemy.JwtApp.Front.Models;
+
+namespace Udemy.JwtApp.Front.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IHttpContextFactory _httpClientFactory;
+
+        public AccountController(IHttpContextFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult SignIn(UserLoginModel model)
+        {
+            return View();
+        }
+    }
+}
